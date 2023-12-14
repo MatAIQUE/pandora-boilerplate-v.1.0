24f7eb9888dcf6f1400cbf5fb03d140f9824a522
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2023-12-14 16:24:32 +0800

    add react-icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Badge, Button } from "./components";
 import Card from "./components/Card";
 import Carousel from "./components/Carousel";
+import { FaHome, FaAngleLeft } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,12 +9,17 @@ export default function Home() {
       <div className="h-screen flex flex-col w-full text-center">
         {/* Page Controller */}
         {/* Add .hidden when controllers should be hidden */}
-        <div className="py-2">1</div>
+        <div className="py-2">
+          <div className="py-4 p-5 flex justify-between">
+            <FaAngleLeft color="white" size={30} />
+            <FaHome color="white" size={30} />
+          </div>
+        </div>
         {/* End of Page Controller */}
 
         {/* Ads Container */}
         <div className="flex flex-col my-10 justify-center items-start mx-10">
-          <Carousel/>
+          <Carousel />
         </div>
         {/* End of Ads Container */}
 
