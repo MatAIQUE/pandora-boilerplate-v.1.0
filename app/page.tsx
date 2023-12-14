64bd1d8dd43116823b11ce7dfdba663ca2a85bbb
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2023-12-14 14:24:08 +0800

    created dynamic page container

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,27 @@ import Card from "./components/Card";
 export default function Home() {
   return (
     <>
-    <div className="p-5">
-      <Card/>
+    <div className="h-screen flex flex-col w-full text-center">
+      
+      {/* Page Controller */}
+        {/* Add .hidden when controllers should be hidden */}
+        <div className="py-2">
+          1
+        </div>
+      {/* End of Page Controller */}
+      
+      {/* Ads Container */}
+        <div className="flex flex-auto justify-center items-start">
+          2
+        </div>
+      {/* End of Ads Container */}
+      
+      {/* Page Content */}
+        <div className="basis-2/4 flex flex-auto justify-center items-start">
+          <Card/>
+        </div>
+      {/* End of Page Content */}
+
     </div>
     </>
     
