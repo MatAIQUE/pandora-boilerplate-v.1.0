f8ab2d8f5c9e3332ee9d9a822e9a4ed8739e845f
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2023-12-19 19:11:05 +0800

    Refactor/ LabelTitle component add label props

--- app/components/LabelTitle.tsx
+++ app/components/LabelTitle.tsx
@@ -1,7 +1,10 @@
-const LabelTitle = () => {
+interface Props {
+  label: string;
+}
+const LabelTitle = ({ label }: Props) => {
   return (
     <div className="text-white font-medium mb-5 flex justify-start">
-      <span className="text-2xl">Enter your membership details</span>
+      <span className="text-2xl">{label}</span>
     </div>
   );
 };
