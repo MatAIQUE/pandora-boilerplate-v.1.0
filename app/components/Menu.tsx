fe9a7f3a673c2bc94f78e624e2f03c71cca649cc
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2023-12-27 17:38:07 +0800

    feat:/ add router back to menu component

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { FaHome, FaAngleLeft } from "react-icons/fa";
 
 const Menu = () => {
+  const router = useRouter();
+  const onNavigateBack = () => {
+    router.back();
+  };
+
   return (
     <div className="my-4 absolute w-full">
       <div className="px-5 flex justify-between">
-        <button className="btn btn-accent">
+        <button onClick={onNavigateBack} className="btn btn-accent">
           <FaAngleLeft color="white" size={30} />
         </button>
         <Link href="/">
