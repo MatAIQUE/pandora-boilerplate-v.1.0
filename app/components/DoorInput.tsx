f163a6a9f3d4e5a92f32abac27301810a42b104a
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2024-01-02 12:09:21 +0800

    added maxlength on doorInput

--- app/components/DoorInput.tsx
+++ app/components/DoorInput.tsx
@@ -4,6 +4,7 @@ const DoorInput = () => {
   return (
     <div className="">
       <input
+        maxLength={2}
         type="text"
         placeholder="0 1"
         className="input input-bordered text-2xl input-secondary w-20 text-center bg-white text-black"
