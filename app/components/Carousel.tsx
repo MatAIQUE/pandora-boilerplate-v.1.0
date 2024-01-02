f203530e3dcec51c2400bc7e3ce5c6c908c4c3ea
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2024-01-02 14:18:22 +0800

    Refactor:/ Carousel component dynamic images pass as props

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,49 +1,52 @@
-import React from "react";
-import display1 from "../assets/ads/display-1.png";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
-const Carousel = () => {
+interface Props {
+  images: Array<{ id: string; url: string }>;
+}
+
+const Carousel = ({ images }: Props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <div className="flex flex-col mt-20 justify-center items-start mx-10 pointer-events-none">
         <div className="carousel rounded-box w-full pointer-events-none">
-          {/* 2048 x 800 */}
-          <div id="item1" className="carousel-item pointer-events-none">
-            <Image
-              src={display1}
-              alt="loc"
-              className="pointer-events-none"
-              height={350}
-              width={992}
-            />
-          </div>
-          <div id="item1" className="carousel-item">
-            <Image
-              src={display1}
-              alt="loc"
-              className=""
-              height={350}
-              width={992}
-            />
-          </div>
-          <div id="item1" className="carousel-item">
-            <Image
-              src={display1}
-              alt="loc"
-              className=""
-              height={350}
-              width={992}
-            />
-          </div>
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${
+                currentIndex === index ? "" : "hidden"
+              }`}
+            >
+              <Image
+                src={image.url}
+                alt={`Image ${index + 1}`}
+                className="pointer-events-none"
+                height={350}
+                width={992}
+              />
+            </div>
+          ))}
         </div>
         <ul className="mt-5 steps mx-auto">
-          <li
-            data-content="#item1"
-            className="p-1 mx-1.5 bg-primary rounded-xl"
-          ></li>
-          <li data-content="" className="p-1 mx-1.5 bg-white rounded-xl"></li>
-          <li data-content="" className="p-1 mx-1.5 bg-white rounded-xl"></li>
-          <li data-content="" className="p-1 mx-1.5 bg-white rounded-xl"></li>
+          {images.map((_, index) => (
+            <li
+              key={index}
+              data-content={`#${index}`}
+              className={`p-1 mx-1.5 ${
+                currentIndex === index ? "bg-primary" : "bg-white"
+              } rounded-xl`}
+            ></li>
+          ))}
         </ul>
       </div>
     </>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import React from "react";
-import Menu from "../components/Menu";
+import { useRouter } from "next/navigation";
 import Carousel from "../components/Carousel";
 import Card from "./_components/Card";
-import { useRouter } from "next/navigation";
 
 const HomePage = () => {
   const router = useRouter();
@@ -16,14 +14,30 @@ const HomePage = () => {
     router.push("/lockers/open");
   };
 
+  // MOCK DATA ONLY
+  const images = [
+    {
+      id: "1",
+      url: "https://res.cloudinary.com/dym23akc6/image/upload/v1674445473/cld-sample-3.jpg",
+    },
+    {
+      id: "2",
+      url: "https://res.cloudinary.com/dym23akc6/image/upload/v1674445472/cld-sample.jpg",
+    },
+    {
+      id: "3",
+      url: "https://res.cloudinary.com/dym23akc6/image/upload/v1674445474/cld-sample-5.jpg",
+    },
+  ];
+
   return (
     <div className="h-screen relative flex flex-col w-full text-center">
-      <Carousel />
+      <Carousel images={images} />
       <div className="basis-2/4 flex flex-auto justify-center items-center mb-96">
         <Card
           onNavigate={onNavigate}
           onNavigateToOpenLocker={onNavigateToOpenLocker}
-          title="Good Morning!"
+          title=""
           subtitle=""
         />
       </div>
