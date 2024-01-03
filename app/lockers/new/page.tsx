2d92ccb243de24dbcbc2e66c834ac6b86b17e144
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2024-01-03 18:23:49 +0800

    trial for on screen kbd

--- app/lockers/new/page.tsx
+++ app/lockers/new/page.tsx
@@ -61,6 +61,53 @@ const GetLockers = () => {
           </div>
         </div>
       </div>
+      <div className="mb-20">
+        <div className="flex justify-center gap-1 my-1 w-full mb-4">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">1</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">2</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">3</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">4</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">5</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">6</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">7</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">8</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">9</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-muted text-neutral">0</kbd>
+        </div>
+        <div className="flex justify-center gap-1 my-1 w-full">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">q</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">w</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">e</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">r</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">t</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">y</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">u</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">i</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">o</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">p</kbd>
+        </div> 
+        <div className="flex justify-center gap-1 my-1 w-full">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">a</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">s</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">d</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">f</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">g</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">h</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">j</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">k</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">l</kbd>
+        </div> 
+        <div className="flex justify-center gap-1 my-1 w-full">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">z</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">x</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">c</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">v</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">b</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">n</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">m</kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-neutral text-white">/</kbd>
+        </div>
+      </div>
     </div>
   );
 };
