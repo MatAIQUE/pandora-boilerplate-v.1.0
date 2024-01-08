934ebb065bf7d2b5f77bd09c06a32b12abfd639f
Tan Matt <matt.tan@aiqueinnovation.com>
2024-01-08 18:18:54 +0800

    update keyboard component backspace position

--- app/components/Keyboard.tsx
+++ app/components/Keyboard.tsx
@@ -1,61 +1,132 @@
 import { FaArrowLeft } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 
 const Keyboard = () => {
-return (
+  return (
     <>
-    <div className="mb-20 w-full justify-center bottom-0 fixed ">
-        <div className="flex justify-end me-36 gap-1 my-1 mb-4">
-            <kbd className="kbd-lg scale-150 m-4 btn border-white bg-transparent"><FaArrowLeft/>BckSpc</kbd>
+      <div className="mb-20 w-full justify-center bottom-0 fixed ">
+        <div className="flex justify-center gap-2 my-2 mb-4">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            1
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            2
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            3
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            4
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            5
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            6
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            7
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            8
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            9
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            0
+          </kbd>
         </div>
-        <div className="flex justify-center gap-1 my-1 mb-4">
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">1</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">2</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">3</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">4</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">5</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">6</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">7</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">8</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">9</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">0</kbd>
+        <div className="flex justify-center gap-2 my-2">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            Q
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            W
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            E
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            R
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            T
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            Y
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            U
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            I
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            O
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            P
+          </kbd>
         </div>
-        <div className="flex justify-center gap-1 my-1">
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">q</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">w</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">e</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">r</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">t</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">y</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">u</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">i</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">o</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">p</kbd>
-        </div> 
-        <div className="flex justify-center gap-1 my-1">
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">a</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">s</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">d</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">f</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">g</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">h</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">j</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">k</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">l</kbd>
-        </div> 
-        <div className="flex justify-center gap-1 my-1">
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">z</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">x</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">c</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">v</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">b</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">n</kbd>
-            <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral btn-disabled">m</kbd>
+        <div className="flex justify-center gap-2 my-2">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            A
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            S
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            D
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            F
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            G
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            H
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            J
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            K
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            L
+          </kbd>
         </div>
-    </div>
+        <div className="flex justify-center gap-2 my-1">
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            Z
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            X
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            C
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            V
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            B
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            N
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral ">
+            M
+          </kbd>
+          <kbd className="kbd-lg scale-150 m-4 btn bg-white text-neutral">
+            <FaArrowLeft />
+          </kbd>
+        </div>
+      </div>
     </>
-);
+  );
+};
 
-
-}
-
-export default Keyboard
+export default Keyboard;
