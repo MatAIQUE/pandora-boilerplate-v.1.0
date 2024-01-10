3d6f55a14b9ffaaa3a6f0b713cfc672730ab3f98
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2024-01-10 17:21:01 +0800

    aded paddings and redirect timeout on error page

--- app/lockers/open/alert/page.tsx
+++ app/lockers/open/alert/page.tsx
@@ -1,10 +1,33 @@
+"use client";
 import { Button } from "@/app/components";
 import LabelTitle from "@/app/components/LabelTitle";
 import Logo from "@/app/components/Logo";
 import React from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { FaBellSlash } from "react-icons/fa";
 
 const AlertPage = () => {
+  const [count, setCount] = useState(100);
+  const [timer, setTimer] = useState(5);
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      if (count > 0 && timer > 0) {
+        setCount(count - 20);
+        setTimer(timer - 1);
+      } else {
+        clearInterval(countdownInterval);
+        router.push("/");
+      }
+    }, 2000);
+
+    return () => clearInterval(countdownInterval); // Cleanup on component unmount
+  }, [count, timer]);
+  const router = useRouter();
+  const onNavigate = () => {
+    router.push("/");
+  };
   return (
     <div className="h-screen relative flex flex-col w-full text-center">
       {/* <Menu /> */}
@@ -21,13 +44,13 @@ const AlertPage = () => {
                   </div>
                   <div className="text-center w-full">
                     <div className="w-full">
-                      <div className="w-full justify-center text-left flex pb-5">
+                      <div className="w-full my-2 justify-center text-left flex pb-5">
                         <p className="text-xl text-white">Incorrect PIN</p>
                       </div>
                       <div className="w-full justify-center text-left flex pb-5">
                         <p className="text-white">
-                          Sorry we’ve locked your Locker for 5 Minutes. If you
-                          forgot you’re PIN Code you can reset it below or talk
+                          Sorry we&apos;ve locked your Locker for 5 Minutes. If you
+                          forgot you&apos;re PIN Code you can reset it below or talk
                           to our Admin on Site
                         </p>
                       </div>
