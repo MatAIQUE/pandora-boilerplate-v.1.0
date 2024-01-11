88f3c85b55fdc3f8c085dd0ff571faf6e04b185f
Tan Matt <matt.tan@aiqueinnovation.com>
2024-01-11 10:50:53 +0800

    checking button func

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -66,7 +66,7 @@ const HomePage = () => {
             color="white"
             weight="500"
             outline=""
-            onClick={onNavigate}
+            onClick={() => onNavigate()}
           />
         </div>
       </div>
